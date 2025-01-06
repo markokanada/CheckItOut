0ce050c61884aeb124d1ee63e9f2da91d89de221
csokamarton <csoka.marton2005@gmail.com>
2025-01-06 22:02:39 +0100

    init frontend

--- start.sh
+++ start.sh
@@ -12,9 +12,11 @@ else
     ln -s backend/.env
 fi
 
+
 if ! [ -d "frontend/node_modules" ]; then
-    docker run --rm  -v "$(pwd)/frontend:/app" --entrypoint npm idomi27/vue install
+    docker run --rm -v "$(pwd)/frontend:/app" -w /app node:18-alpine sh -c "npm install -g pnpm && pnpm install"
 fi
+
 
 docker compose up -d
 
