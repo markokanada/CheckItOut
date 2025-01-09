5359cdf4d632df171208d5dc10097966504d7b10
csokamarton <csoka.marton2005@gmail.com>
2025-01-09 15:29:27 +0100

    Header and Layout added to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from './components/Layout'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<main><h1>In development</h1></main>}/>
+      </Routes>
+      <Routes>
+        <Route path='/newTask' element={<main><h1>In development - new task</h1></main>}/>
+      </Routes>
+      <Routes>
+        <Route path='/profile' element={<main><h1>In development - profile</h1></main>}/>
+      </Routes>
+    </Layout>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
 import './index.css'
 import App from './App.jsx'
+import Header from './components/Header.jsx';
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+})
+
+const system = createSystem(config);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ChakraProvider value={system}>
+    <Router>
+      <Header />
+      <App />
+    </Router>
+  </ChakraProvider>
 )
