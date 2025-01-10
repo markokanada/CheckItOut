3ca8804212fdd94611306ea36ae774cb4c7f7e40
molmardo <molnarmark2005@gmail.com>
2025-01-10 04:32:22 +0000

    taskRecording init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,18 @@
 import './App.css'
 import Layout from './components/Layout'
 import { Route, Routes } from 'react-router-dom'
+import TaskRecording from './taskRecording'
 
 function App() {
-
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<main><h1>In development</h1></main>}/>
-      </Routes>
-      <Routes>
-        <Route path='/newTask' element={<main><h1>In development - new task</h1></main>}/>
-      </Routes>
-      <Routes>
-        <Route path='/profile' element={<main><h1>In development - profile</h1></main>}/>
+        <Route path="/" element={<main><h1>In development</h1></main>} />
+        <Route path="/newTask" element={<TaskRecording />} />
+        <Route path="/profile" element={<main><h1>In development - profile</h1></main>} />
       </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
