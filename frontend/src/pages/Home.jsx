e5b4986a89e9bab4833df317f623dd08862b3ebd
csokamarton <csoka.marton2005@gmail.com>
2025-01-14 20:18:47 +0100

    Add newTask button at the bottom of the page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import { Box, Card, Container, Stack } from "@chakra-ui/react"
+import { Box, Button, Card, Container, Stack } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"
 
 const Home = ()=> {
+    const navigate = useNavigate();
     return(
         <Container>
             <Stack>
@@ -20,21 +22,21 @@ const Home = ()=> {
                     <h1>
                         Mai teendő
                     </h1>
-                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "0.2rem"}}>
+                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "5rem"}}>
                         <Card.Body>
                             <Card.Description>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt nostrum eos recusandae fugit quod est nemo explicabo quos obcaecati maiores, cumque neque nihil optio suscipit vero sed molestias iure!
                             </Card.Description>
                         </Card.Body>
                     </Card.Root>
-                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "0.2rem"}}>
+                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "5rem"}}>
                         <Card.Body>
                             <Card.Description>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt nostrum eos recusandae fugit quod est nemo explicabo quos obcaecati maiores, cumque neque nihil optio suscipit vero sed molestias iure!
                             </Card.Description>
                         </Card.Body>
                     </Card.Root>
-                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "0.2rem"}}>
+                    <Card.Root css={{"boxShadow": "7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "marginTop": "0.2rem", "marginLeft": "5rem", "marginRight": "5rem"}}>
                         <Card.Body>
                             <Card.Description>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt nostrum eos recusandae fugit quod est nemo explicabo quos obcaecati maiores, cumque neque nihil optio suscipit vero sed molestias iure!
@@ -55,6 +57,12 @@ const Home = ()=> {
                     </Card.Root>
                 </Box>
             </Stack>
+            <Button 
+                onClick={() => navigate("/newTask")}
+                variant="plain"
+                css={{"float": "right", "marginRight": "0.5rem", 'marginBottom': "0.5rem", "backgroundColor": "#0044D7", "color":"white"}} >
+                +
+            </Button>
         </Container>
     )
 }
