cec8c1837d612607a0e61d354a27926754ec1bab
molmardo <molnarmark2005@gmail.com>
2025-02-12 01:25:35 +0000

    schedules migrations

--- backend/database/migrations/2025_02_04_190105_create_schedules_table.php
+++ backend/database/migrations/2025_02_04_190105_create_schedules_table.php
@@ -12,7 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('schedules', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string("name",255);
+            $table->time("period_of_time");
+            $table->dateTime("deadline");
+            $table->text("description");
+            
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("task_id");
+
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            $table->foreign("task_id")->references("id")->on("tasks")->onDelete("cascade");
+            
             $table->timestamps();
         });
     }
