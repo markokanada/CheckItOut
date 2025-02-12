11b6c6364591f5cc70c25b83aa33f91d63e10f1b
molmardo <molnarmark2005@gmail.com>
2025-02-12 01:44:01 +0000

    ScheduleRequest Store and Update

--- backend/app/Http/Requests/StoreScheduleRequest.php
+++ backend/app/Http/Requests/StoreScheduleRequest.php
@@ -22,7 +22,23 @@ class StoreScheduleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'period_of_time' => 'required|date_format:H:i',
+            'deadline' => 'required|date_format:Y-m-d H:i:s',
+            'description' => 'required|string',
+            'user_id' => 'required|exists:users,id',
+            'task_id' => 'required|exists:tasks,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'period_of_time.required' => 'The period is required.',
+            'deadline.required' => 'Specifying the deadline is mandatory.',
+            'user_id.exists' => 'The specified user does not exist.',
+            'task_id.exists' => 'The specified user does not exist.',
         ];
     }
 }
