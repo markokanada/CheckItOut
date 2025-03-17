425053355c22e7b370b315275970a49e2047f3ec
markobuborek <markokanadateam2@gmail.com>
2025-03-17 13:36:34 +0100

    main rework to View Component usage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
-import './index.css'
-import App from './App.js'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
+import { ChakraProvider, createSystem, defineConfig } from '@chakra-ui/react';
+import './index.css';
+import App from './App';
 import Header from './components/Header';
+import { useEffect, useState } from 'react';
 
 const config = defineConfig({
   theme: {
@@ -11,15 +12,30 @@ const config = defineConfig({
       colors: {},
     },
   },
-})
+});
 
 const system = createSystem(config);
+
+const AppWrapper = () => {
+  const navigate = useNavigate();
+  const [appInstance, setAppInstance] = useState<App | null>(null);
+
+  useEffect(() => {
+    setAppInstance(new App(navigate)); 
+  }, [navigate]);
+
+  return (
+    <>
+      <Header />
+      {appInstance && appInstance.View()}
+    </>
+  );
+};
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider value={system}>
     <Router>
-      <Header />
-      <App />
+      <AppWrapper />
     </Router>
   </ChakraProvider>
-)
+);
