d4dc4c3b59e71fa889e10a5dce3fe78741913a4f
markobuborek <markokanadateam2@gmail.com>
2025-03-17 13:35:52 +0100

    enable babel plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host: true
-  }
-})
+  plugins: [
+      react({
+          babel: {
+              plugins: [
+                  [
+                      "@babel/plugin-proposal-decorators",
+                      {
+                          version: "2023-05"
+                      }
+                  ]
+              ]
+          }
+      })
+  ]
+});
