ea888ddf16d1602158b65adcb8b7fbeda87cf4b1
markobuborek <markokanadateam2@gmail.com>
2025-03-17 13:36:25 +0100

    App rework to View Class Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,33 @@
-import './App.css'
-import Layout from './components/Layout'
-import { Route, Routes } from 'react-router-dom'
-import TaskRecording from './pages/taskRecording'
-import Home from './pages/Home'
+import './App.css';
+import Layout from './components/Layout';
+import { NavigateFunction, Route, Routes } from 'react-router-dom';
+import TaskRecording from './pages/taskRecording';
+import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import { Component } from 'react';
+import { computed, makeObservable } from 'mobx';
+import ViewComponent from './interfaces/ViewComponent';
 
+export default class App implements ViewComponent{
+  constructor(private navigate: NavigateFunction ) {
+    makeObservable(this);
+  }
 
-function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/newTask" element={<TaskRecording />} />
-      </Routes>
-      <Routes>
-        <Route path='/profile' element={<main><h1>In development - profile</h1></main>} />
-      </Routes>
+  @computed get login(): ViewComponent {
+    return new Login(this.navigate); 
+  }
 
-      <Routes>
-        <Route path='/register' element={<Register />} />
-      </Routes>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Layout>
-  );
-}
-
-export default App;
+  View = () =>
+     (
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/newTask' element={<TaskRecording />} />
+          <Route path='/profile' element={<main><h1>In development - profile</h1></main>} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<this.login.View/>} />
+        </Routes>
+      </Layout>
+    );
+  }
