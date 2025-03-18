238e412e62d62accc3aae88dab8ec34887a907d9
csokamarton <csoka.marton2005@gmail.com>
2025-03-18 20:22:56 +0100

    Tasks relationship on users

--- backend/app/Http/Resources/TaskResource.php
+++ backend/app/Http/Resources/TaskResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class TaskResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,9 +16,12 @@ class UserResource extends JsonResource
     {
         return [
             "id" => $this->id,
-            "name" => $this->name,
-            "email" => $this->email,
-            "tasks" => TaskResource::collection($this->whenLoaded("tasks"))
+            "descreption" => $this->descreption,
+            "due" => $this->due_date,
+            "priority" => $this->priority,
+            "status" => $this->status,
+            "category_id" => $this->category_id,
+            "User" => new UserResource($this->whenLoaded("user"))
         ];
     }
 }
