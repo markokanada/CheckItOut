af1cc02eb71a47dce8217d64c781f185d4fe58fe
csokamarton <csoka.marton2005@gmail.com>
2025-03-18 18:43:39 +0100

    Reworked Navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,36 @@
 import { Link } from "react-router-dom";
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { IoIosMenu } from "react-icons/io";
 
 const Header = () => {
-  return(
-    <Box position="sticky" top="0" zIndex="1000" m={5}>
-      <HStack justifyContent="space-around">
-        <Link to="/">Főoldal<br />megtekintése</Link>
-        <Link to="/newTask">Új feladat<br />felvétele</Link>
-        <Link to="/profile">Profil<br />megtekintése</Link>
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <Box backgroundColor="#0044D7" top="0" position="sticky" padding={5}>
+      <HStack display='flex' justifyContent='space-between'>
+        <Heading margin={0} color="#F5F5DC">CheckItOut</Heading>
+        
+        <button onClick={toggleMenu}><IoIosMenu /></button>
+        
       </HStack>
-      
+
+      {
+        isOpen
+        &&
+        <VStack marginTop={5}>
+          <Link to="/home"><button onClick={toggleMenu}>Főoldal<br />megtekintése</button></Link>
+          <Link to="/newTask"><button onClick={toggleMenu}>Új feladat<br />felvétele</button></Link>
+          <Link to="/profile"><button onClick={toggleMenu}>Profil<br />megtekintése</button></Link>
+          <Link to="/"><button onClick={toggleMenu}>Kijelentkezés</button></Link>
+        </VStack>
+      }
+
+
     </Box>
   );
 };
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,15 +1,20 @@
 
 import { Box, Container } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import Header from "./Header";
+import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
     children: ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({children}) => {
+    const location = useLocation();
+    const locationsWithoutHeader = ["/", "/login", "/register"]
     return (
         <Container pt={0}>
-            <Box as="main">
+            { !locationsWithoutHeader.includes(location.pathname) && <Header />}
+            <Box as="main" maxWidth={1280} alignContent="center" margin="auto">
                 {children}
             </Box>
         </Container>
