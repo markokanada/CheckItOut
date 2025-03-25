1132694fa9c67d3fbdef6ad598f889f1505bc223
csokamarton <csoka.marton2005@gmail.com>
2025-03-25 20:40:42 +0100

    Show task

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -31,7 +31,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        return new TaskResource($task->load(["user", "category"]));
     }
 
     /**
--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Task extends Model
 {
@@ -37,5 +38,9 @@ class Task extends Model
 
     public function schedules() : BelongsToMany {
         return $this->belongsToMany(Schedule::class, "task_schedule");
+    }
+
+    public function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
     }
 }
