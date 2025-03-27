e29a0b0e1944eb8a8ef5db4828536cdb0f7da448
csokamarton <csoka.marton2005@gmail.com>
2025-03-27 18:23:29 +0100

    User Interface

---
+++ frontend/src/interfaces/User.ts
@@ -0,0 +1,8 @@
+interface User {
+    id: number | undefined,
+    name: string | undefined,
+    email: string | undefined,
+    role: string | undefined,
+    created_at: any | undefined,
+    updated_at: any | undefined
+}
