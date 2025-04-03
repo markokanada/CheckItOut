d92caa910cc926ed0d93cb357f7fe2bd7da8732b
csokamarton <csoka.marton2005@gmail.com>
2025-04-03 11:04:56 +0200

    ToggleStatus on card

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,13 +1,17 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 import ViewComponent from "../interfaces/ViewComponent";
 import { Card, Heading } from "@chakra-ui/react";
-import { Button, Stack } from '@mui/material';
+import { Alert, Button, Snackbar, Stack } from '@mui/material';
 import GlobalEntities from "../store/GlobalEntities";
+import { observer } from "mobx-react-lite";
 
 
 export class BaseCard implements ViewComponent {
     task: Task;
-    category: Category; 
+    category: Category;
+    showAlert: boolean = false;
+    alertMessage: string = "";
+    alertType: string = "";
 
     constructor(task: Task) {
         this.task = task;
@@ -17,10 +21,39 @@ export class BaseCard implements ViewComponent {
         this.category = GlobalEntities.categories[idx];
 
         makeObservable(this, {
-            task: observable
+            task: observable,
+            toggleStatus: action,
+            showAlert: observable,
+            alertMessage: observable,
+            handleClose: action,
+            alertType: observable
         })
     }
-    View = () => (
+
+    @action toggleStatus = async (newStatus: string) => {
+        this.task.status = newStatus;
+        this.task.user_id = (GlobalEntities.user.id as number);
+
+        const resp = await GlobalEntities.updateTask(this.task);
+        if (resp.status === 200) {
+            this.toggleAlert(true, `Állapot sikeresen módosítva: ${newStatus}`, "success");
+        }
+        else {
+            this.toggleAlert(true, "Sikertelen módosítás", "error");
+        }
+    }
+
+    @action handleClose = () => {
+        this.toggleAlert(false, "", "");
+    }
+
+    @action toggleAlert = (open: boolean, message: string, type: string) => {
+        this.showAlert = open;
+        this.alertMessage = message;
+        this.alertType = type;
+    }
+
+    View = observer( () => (
         <Card.Root css={{ "boxShadow": "7px 7px 7px 7px rgb(0,0,0,0.5)", "borderRadius": "0.5rem", "margin": "5rem" }}>
             <Card.Body>
                 <Card.Header>
@@ -33,21 +66,28 @@ export class BaseCard implements ViewComponent {
                     {this.task.due_date.toString()}
                 </Card.Description>
                     Állapot: {this.task.status} | Prioritás: {this.task.priority}
-                <Card.Description>
-                
-                    Kategória: {this.category.name}
-                
+                <Card.Description>                
+                    Kategória: {this.category.name}                
                 </Card.Description>
             </Card.Body>
             <Card.Footer display="flex" justifyContent="flex-end" >
                 <Stack spacing={2.5} direction="row" margin={"1rem"}>
-                    <Button variant="contained" color="warning">Folyamatban</Button>
-                    <Button variant="contained" color="success">Kész</Button>
+                    <Button onClick={() => this.toggleStatus("folyamatban")} variant="contained" color="warning">Folyamatban</Button>
+                    <Button onClick={() => this.toggleStatus("kész")} variant="contained" color="success">Kész</Button>
                 </Stack>
 
             </Card.Footer>
+            
+            <Snackbar 
+                open={this.showAlert} 
+                autoHideDuration={6000} 
+                onClose={this.handleClose}
+            >
+                <Alert variant="filled">{this.alertMessage}</Alert>
+            </Snackbar>
+            
         </Card.Root>
-    );
+    ));
 
 
 
