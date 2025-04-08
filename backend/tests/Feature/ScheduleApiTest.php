fe751a6d2aaeefc5ce21011e69fc00037789201b
markokanada <markokanadateam2@gmail.com>
2025-04-08 19:01:46 +0000

    test_schedule_index

--- backend/tests/Feature/ScheduleApiTest.php
+++ backend/tests/Feature/ScheduleApiTest.php
@@ -10,5 +10,18 @@ class ScheduleApiTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_schedule_index(): void
+    {
+        $n = 5;
+        $schedules = Schedule::factory($n)->create();
+        $firstSchedule = $schedules->first();
 
+        $response = $this->getJson('/api/schedules');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount($n, 'data');
+        $response->assertJsonPath('data.0.id', $firstSchedule->id);
+        $response->assertJsonPath('data.0.title', $firstSchedule->title);
+        $response->assertJsonPath('data.0.deadline', $firstSchedule->deadline);
+    }
 }
