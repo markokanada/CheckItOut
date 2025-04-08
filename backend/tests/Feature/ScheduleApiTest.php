f0b816c4ab1b3af8a3d6945e923a46f9ebf3e732
markokanada <markokanadateam2@gmail.com>
2025-04-08 19:01:33 +0000

    ScheduleApiTest init

---
+++ backend/tests/Feature/ScheduleApiTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Schedule;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ScheduleApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+}
