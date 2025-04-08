0d8fd4c9b2c4b98bf25a590c256322ce60108a65
markokanada <markokanadateam2@gmail.com>
2025-04-08 18:52:13 +0000

    TaskTest init

---
+++ backend/tests/Unit/TaskTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    
+}
