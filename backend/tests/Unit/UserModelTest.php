7a6229f91a7b8aa9100f56974c971542316ee2b0
markokanada <markokanadateam2@gmail.com>
2025-04-08 18:57:39 +0000

    UserModelTest Init

---
+++ backend/tests/Unit/UserModelTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Schedule;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use PHPUnit\Framework\TestCase;
+
+class UserModelTest extends TestCase
+{
+
+}
