4d725433fe276c923f1fec032c5e1ada79c042b0
csokamarton <csoka.marton2005@gmail.com>
2025-04-14 07:24:07 +0000

    Edit profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,13 +4,15 @@ import { Button, Container, FormControl, Stack, TextField } from "@mui/material"
 import GlobalEntities from "../store/GlobalEntities";
 import { NavigateFunction } from "react-router-dom";
 import { action, makeObservable, observable } from "mobx";
+import { FormEvent } from "react";
 
 export default class Profile implements ViewComponent {
 
     constructor(public naviagte: NavigateFunction) {
         makeObservable(this, {
             editable: observable,
-            toggleEdit: action
+            toggleEdit: action,
+            
         })
     }
 
@@ -18,22 +20,43 @@ export default class Profile implements ViewComponent {
 
     @action toggleEdit = () => {
         this.editable = !this.editable;
+    }
+
+
+
+    @action submitEdit = (e: any) => {
+        e.preventDefault();
+       
+        console.log(e.target.name.value);
         
     }
 
     View = observer(() => (
-        <Container>
-            <Stack direction={"column"}>
-                <FormControl>
-                    <TextField id="name" label="Felhasználó név" variant="filled" value={GlobalEntities.user.name} disabled={!this.editable}/>
-                    <TextField id="email" label="E-mail cím" variant="filled" value={GlobalEntities.user.email} disabled={!this.editable}/>
-                </FormControl>
-            </Stack>
-            <Stack>
-                <Button sx={{ margin: "auto" }} variant='contained' onClick={this.toggleEdit}>
-                    Szerkesztés
-                </Button>
-            </Stack>
+        <Container sx={{ marginY: "2rem" }}>
+            <form onSubmit={this.submitEdit}>
+                <Stack direction={"column"}>
+                    <FormControl>
+                        <TextField id="name" sx={{ paddingBottom: 3 }} label="Felhasználó név" variant="filled" defaultValue={GlobalEntities.user.name} disabled={!this.editable} />
+                        <TextField id="email" label="E-mail cím" variant="filled" defaultValue={GlobalEntities.user.email} disabled={!this.editable} />
+                    </FormControl>
+                </Stack>
+                <Stack sx={{ marginTop: "2rem" }} direction={"row"}>
+                    {
+                        this.editable
+                            ?
+                            <><Button sx={{ margin: "auto" }} variant='contained' onClick={this.toggleEdit}>
+                                Mégse
+                            </Button><Button sx={{ margin: "auto" }} variant='contained' type="submit">
+                                    Mentés
+                                </Button></>
+                            :
+                            <Button sx={{ margin: "auto" }} variant='contained' onClick={this.toggleEdit}>
+                                Szerkesztés
+                            </Button>
+                    }
+
+                </Stack>
+            </form>
         </Container>
     ));
 }
