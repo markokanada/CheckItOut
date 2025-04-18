94be22033e003a7a029bba6eb102517309ac3ba1
markobuborek <markokanadateam2@gmail.com>
2025-04-18 12:24:32 +0200

    Frontend: Remove unused css from Login ViewComponent

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import React, { Component, FormEvent, ChangeEvent } from "react";
 import { makeObservable, observable, action, computed } from "mobx";
 import {  NavigateFunction } from "react-router-dom";
-import "./css/Login.css";
 import ViewComponent from "../interfaces/ViewComponent";
 import { Box, Heading, Input, Button, Text, Link } from "@chakra-ui/react";
 import { FormControl, FormLabel } from "@mui/material";
