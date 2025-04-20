9af57167f2307f4823786051ae1e08198ed62754
markobuborek <markokanadateam2@gmail.com>
2025-04-20 17:44:27 +0200

    Frontend: Landy Init - Updated Landing Page with new Components

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -20,82 +20,71 @@ import ScrollToTop from "../common/ScrollToTop";
 import ContentBlock from "../components/ContentBlock";
 import MiddleBlock from "../components/MiddleBlock";
 import ContactForm from "../components/ContactForm";
+import { Styles } from "../styles/styles";
+import Footer from "../components/Footer";
+import IntroContent from "../content/IntroContent.json";
+import MiddleBlockContent from "../content/MiddleBlockContent.json";
+import AboutContent from "../content/AboutContent.json";
+import MissionContent from "../content/MissionContent.json";
+import ProductContent from "../content/ProductContent.json";
+import ContactContent from "../content/ContactContent.json";
+import Header from "../components/Header/index";
+
 export default class Landing implements ViewComponent {
   constructor(public navigate: NavigateFunction) {}
 
-  View = () => {
-    console.log("a")
-    const features = [
-      {
-        icon: FaCheckCircle,
-        title: "Egyszerű használat",
-        description: "Intuitív felület gyors, hatékony napi menedzsmenthez.",
-      },
-      {
-        icon: FaListUl,
-        title: "Személyre szabhatóság",
-        description: "Egyedi listák, kategóriák, színek – minden, ahogy te szeretnéd.",
-      },
-      {
-        icon: FaClock,
-        title: "AI-időbeosztás",
-        description: "Töltse ki az üres óráidat hasznosan a beépített intelligens ütemező.",
-      },
-      {
-        icon: FaBell,
-        title: "Intelligens értesítések",
-        description: "Nem csak emlékeztet, hanem javasol is – tanul a szokásaidból.",
-      },
-    ];    
+  View = () => {    
 
     return (
-      <Container>
+      // <>
+      //   <Styles />
+      //   <Header />
+        <Container>
       <ScrollToTop />
       <ContentBlock
         direction="right"
-        title={"IntroContent.title"}
-        content={"IntroContent.text"}
-        button={[
-          { title: "title" },
-        ]}
+        title={IntroContent.title}
+        content={IntroContent.text}
+        button={IntroContent.button}
         icon="developer.svg"
         id="intro"
       />
       <MiddleBlock
-        title={"MiddleBlockContent.title"}
-        content={"MiddleBlockContent.text"}
-        button={"MiddleBlockContent.button"}
+        title={MiddleBlockContent.title}
+        content={MiddleBlockContent.text}
+        button={MiddleBlockContent.button}
       />
       <ContentBlock
         direction="left"
-        title={"AboutContent.title"}
-        content={"AboutContent.text"}
-        section={[
-          { title: "title", content: "content", icon:"icon" },
-        ]}
+        title={AboutContent.title}
+        content={AboutContent.text}
+        section={AboutContent.section}
         icon="graphs.svg"
         id="about"
       />
       <ContentBlock
         direction="right"
-        title={"MissionContent.title"}
-        content={"MissionContent.text"}
+        title={MissionContent.title}
+        content={MissionContent.text}
         icon="product-launch.svg"
         id="mission"
       />
       <ContentBlock
         direction="left"
-        title={"ProductContent.title"}
-        content={"ProductContent.text"}
+        title={ProductContent.title}
+        content={ProductContent.text}
         icon="waving.svg"
         id="product"
       />
       <ContactForm
-        title={"ContactContent.title"}
-        content={"ContactContent.text"}
+        title={ContactContent.title}
+        content={ContactContent.text}
         id="contact"
       />
-    </Container>
+    </Container>  
+    // <Footer />
+    // </>
+
     );
   };
 }
