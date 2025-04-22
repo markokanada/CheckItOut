ebd964a133d26d90875fbfc2180eb7379f7458be
markobuborek <markokanadateam2@gmail.com>
2025-04-22 13:39:49 +0200

    Frontend: Update Documentation page to react with language changes

--- frontend/src/pages/Documentation.tsx
+++ frontend/src/pages/Documentation.tsx
@@ -1,28 +1,27 @@
 import { makeObservable } from "mobx";
 import { observer } from "mobx-react-lite";
 import { NavigateFunction } from "react-router-dom";
+import { useTranslation } from "react-i18next"; // 🔹 HOOK IMPORTÁLÁSA
 import ViewComponent from "../interfaces/ViewComponent";
 
 export default class Documentation implements ViewComponent {
-    
-    constructor(public navigate: NavigateFunction) {
-        
-        makeObservable(this, {
-            
-        });
-    }
-     View = observer(() => (
-        <iframe
-          src="https://organic-gong-e92.notion.site/ebd/1b93a97be88580babc93e39736ae7dcb"
-          style={{
-            width: "100%",
-            height: "120vh",
-            border: "none",
-          }}
-          allowFullScreen
-        />
-      ));
+  constructor(public navigate: NavigateFunction) {
+    makeObservable(this, {});
+  }
 
+  View = observer(() => {
+    const { t } = useTranslation();
 
-
+    return (
+      <iframe
+        src={t("Documentation Link")} 
+        style={{
+          width: "100%",
+          height: "120vh",
+          border: "none",
+        }}
+        allowFullScreen
+      />
+    );
+  });
 }
