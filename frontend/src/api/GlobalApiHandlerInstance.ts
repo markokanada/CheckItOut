968e3683c10f58b17d7ce5286c435084df38b674
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:51 +0200

    Frontend: Prettier format frontend/src/api/GlobalApiHandlerInstance.ts

--- frontend/src/api/GlobalApiHandlerInstance.ts
+++ frontend/src/api/GlobalApiHandlerInstance.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const GlobalApiHandlerInstance = axios.create({
-    baseURL: "http://backend.vm1.test/api",
-    headers:{
-        "Accept": "application/json",
-        "Content-Type": "application/json" 
-    }
+  baseURL: "http://backend.vm1.test/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default GlobalApiHandlerInstance;
