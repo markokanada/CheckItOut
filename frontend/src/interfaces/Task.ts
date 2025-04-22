cfd7ddb36385b8cb2879710a301c3141f879e825
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:52 +0200

    Frontend: Prettier format frontend/src/interfaces/Task.ts

--- frontend/src/interfaces/Task.ts
+++ frontend/src/interfaces/Task.ts
@@ -1,10 +1,10 @@
 interface Task {
-    id: number,
-    title: string,
-    description: string,
-    due_date: Date,
-    priority: number,
-    status: string,
-    category_id: number,
-    user_id: number | object
+  id: number;
+  title: string;
+  description: string;
+  due_date: Date;
+  priority: number;
+  status: string;
+  category_id: number;
+  user_id: number | object;
 }
