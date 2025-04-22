d40c75d99d3badd94d04f692d09ab194771b5b05
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:52 +0200

    Frontend: Prettier format frontend/src/main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, useNavigate } from 'react-router-dom';
-import { ChakraProvider, createSystem, defineConfig } from '@chakra-ui/react';
-import './index.css';
-import App from './App';
-import { useEffect, useState } from 'react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './translation';
-import 'antd/dist/antd.min.css';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
+import { useEffect, useState } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./translation";
+import "antd/dist/antd.min.css";
 
 const config = defineConfig({
   theme: {
@@ -23,25 +23,18 @@ const AppWrapper = () => {
   const [appInstance, setAppInstance] = useState<App | null>(null);
 
   useEffect(() => {
-    setAppInstance(new App(navigate)); 
+    setAppInstance(new App(navigate));
   }, [navigate]);
 
-  return (
-    <>
-      {appInstance && appInstance.View()}
-    </>
-  );
+  return <>{appInstance && appInstance.View()}</>;
 };
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider value={system}>
     <BrowserRouter>
-    <I18nextProvider i18n={i18n}>
-    
-    </I18nextProvider>
+      <I18nextProvider i18n={i18n}></I18nextProvider>
 
       <AppWrapper />
     </BrowserRouter>
-  </ChakraProvider>
-
+  </ChakraProvider>,
 );
