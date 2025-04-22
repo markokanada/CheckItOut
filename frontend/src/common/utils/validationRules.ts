aeb060578438f4ea7c1d9017f7b36973ab51c4a7
markobuborek <markokanadateam2@gmail.com>
2025-04-22 14:25:01 +0200

    Frontend: Update Validation Rules to use translations

--- frontend/src/common/utils/validationRules.ts
+++ frontend/src/common/utils/validationRules.ts
@@ -1,18 +1,20 @@
+import i18next from "i18next"; // 🔹 Ezt használd hook helyett
 import { validateProps } from "../../common/types";
 
 export default function validate(values: validateProps) {
   let errors = {} as validateProps;
 
   if (!values.name) {
-    errors.name = "Name is required";
+    errors.name = i18next.t("Name Error");
   }
   if (!values.email) {
-    errors.email = "Email address is required";
+    errors.email = i18next.t("Email Error1");
   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-    errors.email = "Email address is invalid";
+    errors.email = i18next.t("Email Error2");
   }
   if (!values.message) {
-    errors.message = "Message is required";
+    errors.message = i18next.t("Message Error");
   }
+
   return errors;
 }
