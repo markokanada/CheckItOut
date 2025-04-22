8a02a3b9887a64fca33f2c2357773bc009a059cd
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:52 +0200

    Frontend: Prettier format frontend/src/interfaces/User.ts

--- frontend/src/interfaces/User.ts
+++ frontend/src/interfaces/User.ts
@@ -1,8 +1,8 @@
 interface User {
-    id: number | undefined,
-    name: string | undefined,
-    email: string | undefined,
-    role: string | undefined,
-    created_at: any | undefined,
-    updated_at: any | undefined
+  id: number | undefined;
+  name: string | undefined;
+  email: string | undefined;
+  role: string | undefined;
+  created_at: any | undefined;
+  updated_at: any | undefined;
 }
