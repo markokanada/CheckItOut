560dc6581b4ad78fcb3964b34681e692bbfd7e4c
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:51 +0200

    Frontend: Prettier format frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,33 @@
-import './App.css';
-import Layout from './components/Layout';
-import { NavigateFunction, Route, Routes } from 'react-router-dom';
-import TaskRecording from './pages/taskRecording';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Landing from './pages/Landing';
-import { computed, makeObservable } from 'mobx';
-import ViewComponent from './interfaces/ViewComponent';
-import GlobalEntities from './store/GlobalEntities';
-import Profile from './pages/Profile';
-import UserManagement from './pages/UserManagement';
-import { Suspense } from 'react';
-import { Styles } from './styles/styles';
-import Header from './components/Header/index';
-import Footer from './components/Footer';
-import Documentation from './pages/Documentation';
+import "./App.css";
+import Layout from "./components/Layout";
+import { NavigateFunction, Route, Routes } from "react-router-dom";
+import TaskRecording from "./pages/taskRecording";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Landing from "./pages/Landing";
+import { computed, makeObservable } from "mobx";
+import ViewComponent from "./interfaces/ViewComponent";
+import GlobalEntities from "./store/GlobalEntities";
+import Profile from "./pages/Profile";
+import UserManagement from "./pages/UserManagement";
+import { Suspense } from "react";
+import { Styles } from "./styles/styles";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer";
+import Documentation from "./pages/Documentation";
 
-export default class App implements ViewComponent{
-  constructor(private navigate: NavigateFunction ) {
+export default class App implements ViewComponent {
+  constructor(private navigate: NavigateFunction) {
     makeObservable(this);
   }
 
   @computed get login(): ViewComponent {
-    return new Login(this.navigate); 
+    return new Login(this.navigate);
   }
 
   @computed get landing(): ViewComponent {
-    return new Landing(this.navigate); 
+    return new Landing(this.navigate);
   }
 
   @computed get documentation(): ViewComponent {
@@ -38,12 +38,12 @@ export default class App implements ViewComponent{
     return new Home(this.navigate);
   }
 
-  @computed get isLoggedIn() : boolean {
-    return (GlobalEntities.user.id != undefined)
+  @computed get isLoggedIn(): boolean {
+    return GlobalEntities.user.id != undefined;
   }
 
-  @computed get taskRecording() : ViewComponent {
-    return new TaskRecording(this.navigate)
+  @computed get taskRecording(): ViewComponent {
+    return new TaskRecording(this.navigate);
   }
 
   @computed get profile(): ViewComponent {
@@ -54,28 +54,33 @@ export default class App implements ViewComponent{
     return new Register(this.navigate);
   }
 
-  
   @computed get userManagement(): ViewComponent {
     return new UserManagement(this.navigate);
   }
 
-  View = () =>
-     (
-      <Suspense fallback={null}>
+  View = () => (
+    <Suspense fallback={null}>
       <Styles />
       <Header />
-        <Routes>
-          <Route path='/' element={<this.landing.View/>} />
-          <Route path='/how-to-use' element={<this.documentation.View/>} />
-          <Route path='/home' element={this.isLoggedIn ? <this.home.View /> : <></>} />
-          <Route path='/newTask' element={<this.taskRecording.View />} />
-          <Route path='/profile' element={/*<main><h1>In development - profile</h1></main>*/ <this.profile.View />} />
-          <Route path='/register' element={<this.register.View />} />
-          <Route path='/login' element={<this.login.View/>} />
-          <Route path='/admin/users' element={<this.userManagement.View />} />
-        </Routes>
-            <Footer />
-      </Suspense>
-
-    );
-  }
+      <Routes>
+        <Route path="/" element={<this.landing.View />} />
+        <Route path="/how-to-use" element={<this.documentation.View />} />
+        <Route
+          path="/home"
+          element={this.isLoggedIn ? <this.home.View /> : <></>}
+        />
+        <Route path="/newTask" element={<this.taskRecording.View />} />
+        <Route
+          path="/profile"
+          element={
+            /*<main><h1>In development - profile</h1></main>*/ <this.profile.View />
+          }
+        />
+        <Route path="/register" element={<this.register.View />} />
+        <Route path="/login" element={<this.login.View />} />
+        <Route path="/admin/users" element={<this.userManagement.View />} />
+      </Routes>
+      <Footer />
+    </Suspense>
+  );
+}
