c9d97a564a6ab741b15534de54c49d9d6ed0c345
markobuborek <markokanadateam2@gmail.com>
2025-04-22 16:10:53 +0200

    Frontend: Prettier format frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [
-      react({
-          babel: {
-              plugins: [
-                  [
-                      "@babel/plugin-proposal-decorators",
-                      {
-                          version: "2023-05"
-                      }
-                  ]
-              ]
-          }
-      })
+    react({
+      babel: {
+        plugins: [
+          [
+            "@babel/plugin-proposal-decorators",
+            {
+              version: "2023-05",
+            },
+          ],
+        ],
+      },
+    }),
   ],
-  server:{
+  server: {
     host: true,
-    allowedHosts: ['frontend.vm1.test']
-  }
+    allowedHosts: ["frontend.vm1.test"],
+  },
 });
