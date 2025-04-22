f0d16f3110c8d22c416d3c6df9eca0924bf90a79
markobuborek <markokanadateam2@gmail.com>
2025-04-22 15:08:48 +0200

    Frontend: Update Header Component with language support and with different ui support for user interactions

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,7 +3,6 @@ import { Row, Col, Drawer } from "antd";
 import { withTranslation, TFunction } from "react-i18next";
 import Container from "../../common/Container";
 import { SvgIcon } from "../../common/SvgIcon";
-import { Button } from "../../common/Button";
 import {
   HeaderSection,
   LogoContainer,
@@ -14,100 +13,129 @@ import {
   Label,
   Outline,
   Span,
+  BottomSection,
+  DrawerContent,
+  TopSection,
 } from "./styles";
-import { Center, Text } from "@chakra-ui/react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { LanguageSwitchContainer, LanguageSwitch } from "../Footer/styles";
 import i18n from "../../translation";
+import styled from "styled-components";
+
 
 const Header = ({ t }: { t: TFunction }) => {
   const [visible, setVisibility] = useState(false);
+  const toggleButton = () => setVisibility(!visible);
 
-  const toggleButton = () => {
-    setVisibility(!visible);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isHome = location.pathname === "/";
+  const isDocumentation = location.pathname === "/how-to-use";
+
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id) as HTMLDivElement;
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+    setVisibility(false);
   };
 
-  const MenuItem = () => {
-    const scrollTo = (id: string) => {
-      const element = document.getElementById(id) as HTMLDivElement;
-      element.scrollIntoView({
-        behavior: "smooth",
-      });
-      setVisibility(false);
-    };
-
-    const location = useLocation();
-
-    const isHome = location.pathname === "/";
-    return (
-      <>
-        {isHome && (
-    <>
-      <CustomNavLinkSmall onClick={() => scrollTo("why-us")}>
-        <Span>{t("nav1")}</Span>
-      </CustomNavLinkSmall>
-      <CustomNavLinkSmall onClick={() => scrollTo("features")}>
-        <Span>{t("nav2")}</Span>
-      </CustomNavLinkSmall>
-      <CustomNavLinkSmall onClick={() => scrollTo("usage")}>
-        <Span>{t("nav3")}</Span>
-      </CustomNavLinkSmall>
-      <CustomNavLinkSmall onClick={() => scrollTo("content")}>
-        <Span>{t("nav4")}</Span>
-      </CustomNavLinkSmall>
-    </>
-  )}
-      </>
-    );
-  };
   const handleChange = (language: string) => {
     i18n.changeLanguage(language);
   };
+
+  const MenuItem = () => (
+    <>
+      {isHome && (
+        <>
+          <CustomNavLinkSmall onClick={() => scrollTo("why-us")}>
+            <Span>{t("nav1")}</Span>
+          </CustomNavLinkSmall>
+          <CustomNavLinkSmall onClick={() => scrollTo("features")}>
+            <Span>{t("nav2")}</Span>
+          </CustomNavLinkSmall>
+          <CustomNavLinkSmall onClick={() => scrollTo("usage")}>
+            <Span>{t("nav3")}</Span>
+          </CustomNavLinkSmall>
+          <CustomNavLinkSmall onClick={() => scrollTo("content")}>
+            <Span>{t("nav4")}</Span>
+          </CustomNavLinkSmall>
+        </>
+      )}
+    </>
+  );
+
   return (
     <HeaderSection>
       <Container>
         <Row justify="space-between">
-          
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.png" width="auto"  height="64px" />
+            <SvgIcon src="logo.png" width="auto" height="64px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
+            {(isHome || isDocumentation) && (
+              <>
+                <CustomNavLinkSmall onClick={() => navigate("/register")}>
+                  <Span style={{ color: "#FF824B" }}>{t("Get Started")}</Span>
+                </CustomNavLinkSmall>
+                <CustomNavLinkSmall onClick={() => navigate("/login")}>
+                  <Span style={{ color: "#2D6CDF" }}>{t("Sign In")}</Span>
+                </CustomNavLinkSmall>
+              </>
+            )}
           </NotHidden>
           <Burger onClick={toggleButton}>
             <Outline />
           </Burger>
         </Row>
+
+        {/* Drawer for Mobile Menu */}
         <Drawer closable={false} open={visible} onClose={toggleButton}>
-          <Col style={{ marginBottom: "2.5rem" }}>
-            <Label onClick={toggleButton}>
-              <Col span={12}>
-                <Menu>{t("menu")}</Menu>
+          <DrawerContent>
+            <TopSection>
+              <Col style={{ marginBottom: "2.5rem" }}>
+                <Label onClick={toggleButton}>
+                  <Col span={12}>
+                    <Menu>{t("menu")}</Menu>
+                  </Col>
+                  <Col span={12}>
+                    <Outline />
+                  </Col>
+                </Label>
               </Col>
-              <Col span={12}>
-                <Outline />
-              </Col>
-            </Label>
-          </Col>
-          <MenuItem />
-          <Center><LanguageSwitchContainer>
-                          <LanguageSwitch onClick={() => handleChange("en")}>
-                            <SvgIcon
-                              src="en.svg"
-                              aria-label="homepage"
-                              width="30px"
-                              height="30px"
-                            />
-                          </LanguageSwitch>
-                          <LanguageSwitch onClick={() => handleChange("hu")}>
-                            <SvgIcon
-                              src="hu.svg"
-                              aria-label="homepage"
-                              width="30px"
-                              height="30px"
-                            />
-                          </LanguageSwitch>
-                        </LanguageSwitchContainer></Center>
+              <MenuItem />
+            </TopSection>
+
+            {(isHome || isDocumentation) && (
+              <BottomSection>
+                <CustomNavLinkSmall onClick={() => navigate("/register")}>
+                  <Span style={{ color: "#FF824B" }}>{t("Get Started")}</Span>
+                </CustomNavLinkSmall>
+                <CustomNavLinkSmall onClick={() => navigate("/login")}>
+                  <Span style={{ color: "#2D6CDF" }}>{t("Sign In")}</Span>
+                </CustomNavLinkSmall>
+                <LanguageSwitchContainer>
+                  <LanguageSwitch onClick={() => handleChange("en")}>
+                    <SvgIcon
+                      src="en.svg"
+                      aria-label="english"
+                      width="30px"
+                      height="30px"
+                    />
+                  </LanguageSwitch>
+                  <LanguageSwitch onClick={() => handleChange("hu")}>
+                    <SvgIcon
+                      src="hu.svg"
+                      aria-label="magyar"
+                      width="30px"
+                      height="30px"
+                    />
+                  </LanguageSwitch>
+                </LanguageSwitchContainer>
+              </BottomSection>
+            )}
+          </DrawerContent>
         </Drawer>
       </Container>
     </HeaderSection>
