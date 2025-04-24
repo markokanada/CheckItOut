4c62cb9491c29b5c2d481df90e26574cbdbc6d3a
markobuborek <markokanadateam2@gmail.com>
2025-04-24 17:22:43 +0200

    Frontend: Update MiddleBlock for Landing

--- frontend/src/components/MiddleBlock/index.tsx
+++ frontend/src/components/MiddleBlock/index.tsx
@@ -3,31 +3,34 @@ import { withTranslation, TFunction } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
 import { Button } from "../../common/Button";
 import { MiddleBlockSection, Content, ContentWrapper } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 interface MiddleBlockProps {
   title: string;
   content: string;
   button: string;
   t: TFunction;
+  id: string;
 }
 
-const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
+const MiddleBlock = ({ title, content, button, t, id }: MiddleBlockProps) => {
   const scrollTo = (id: string) => {
     const element = document.getElementById(id) as HTMLDivElement;
     element.scrollIntoView({
       behavior: "smooth",
     });
   };
+  const navigate = useNavigate();
   return (
     <MiddleBlockSection>
       <Slide direction="up" triggerOnce>
-        <Row justify="center" align="middle">
+        <Row id={id} justify="center" align="middle">
           <ContentWrapper>
             <Col lg={24} md={24} sm={24} xs={24}>
               <h6>{t(title)}</h6>
               <Content>{t(content)}</Content>
               {button && (
-                <Button name="submit" onClick={() => scrollTo("mission")}>
+                <Button name="submit" onClick={() => navigate("/register")}>
                   {t(button)}
                 </Button>
               )}
