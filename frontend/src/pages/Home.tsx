279798ff8bbdae18179f41570c89a9c5c486814b
markobuborek <markokanadateam2@gmail.com>
2025-04-26 00:14:57 +0200

    Frontend: new Home Design with desktop view in progess

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,83 +1,99 @@
-import { Box, Button, Card, Container } from "@chakra-ui/react";
-import { Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { NavigateFunction } from "react-router-dom";
 import ViewComponent from "../interfaces/ViewComponent";
 import { action, computed, makeObservable, toJS } from "mobx";
 import GlobalEntities from "../store/GlobalEntities";
 import { BaseCard } from "../components/Card";
 import { observer } from "mobx-react-lite";
+import { Divider } from "antd";
+import { Hidden } from "@mui/material";
+import { EmptyMessage } from "../common/EmptyText";
+import { Section } from "../common/Section";
 
 export default class Home implements ViewComponent {
   constructor(public navigate: NavigateFunction) {
-    makeObservable(this, {
-      showCard: action,
-      card: computed,
-    });
+    makeObservable(this);
   }
 
-  @action showCard = () => {
-    return GlobalEntities.firstTask != undefined;
-  };
-
-  @action createCard = (task: Task) => {
+  @action private createCard = (task: Task) => {
     const card = new BaseCard(task);
     return <card.View />;
   };
 
-  @computed get card() {
-    if (GlobalEntities.firstTask != undefined) {
-      return new BaseCard(GlobalEntities.firstTask).View();
-    }
-    return <></>;
+  @computed private get card() {
+    return GlobalEntities.firstTask ? this.createCard(GlobalEntities.firstTask) : null;
   }
 
-  View = observer(() => (
-    <Container>
-      <Stack>
-        <Box padding={{ md: "5rem", base: "2rem" }}>
-          <h1>Következő teendő</h1>
+  View = observer(() => {
+    const tasks = toJS(GlobalEntities.tasks);
+    const doneTasks = toJS(GlobalEntities.doneTasks);
 
-          {GlobalEntities.firstTask != undefined ? (
-            this.createCard(GlobalEntities.firstTask)
-          ) : (
-            <h3>Nincs megjeleníthető feladat</h3>
-          )}
-        </Box>
-        <Box padding={{ md: "5rem", base: "2rem" }}>
-          <h1>Mai teendő</h1>
+    const columns = useBreakpointValue({ base: 1, md: 2, lg: 2, xl: 3 });
 
-          {toJS(GlobalEntities.tasks).map((task: Task, index) => {
-            const card = new BaseCard(toJS(GlobalEntities.tasks[index]));
+    return (<>
+              <Hidden lgDown></Hidden>
+    <Hidden lgUp>
 
-            return <card.View key={index} />;
-          })}
-        </Box>
-        <Box padding={{ md: "5rem", base: "2rem" }}>
-          <h1>Ma elvégzett teendők</h1>
-          {GlobalEntities.doneTasks.length != 0 ? (
-            toJS(GlobalEntities.doneTasks).map((task: Task, index) => {
-              const card = new BaseCard(task);
 
-              return <card.View key={index} />;
-            })
-          ) : (
-            <h3>Ma még nincs kész feladatot</h3>
-          )}
-        </Box>
-      </Stack>
-      <Button
-        onClick={() => this.navigate("/newTask")}
-        variant="plain"
-        css={{
-          float: "right",
-          marginRight: "0.5rem",
-          marginBottom: "0.5rem",
-          backgroundColor: "#007bff",
-          color: "white",
-        }}
-      >
-        +
-      </Button>
-    </Container>
-  ));
+    <Container maxW="6xl" py={{ base: 6, md: 8 }}>
+        <VStack padding={10} align="stretch">
+          <Section title="Következő teendő">
+            {this.card ?? <EmptyMessage message="Nincs megjeleníthető feladat" />}
+          </Section>
+
+          <Divider />
+
+          <Section title="Mai teendők">
+            {tasks.length ? (
+              <SimpleGrid columns={columns} padding={6}>
+                {tasks.map((task: Task, i) => this.createCard(task))}
+              </SimpleGrid>
+            ) : (
+              <EmptyMessage message="Nincs aktuális feladat" />
+            )}
+          </Section>
+
+          <Divider />
+
+          <Section title="Ma elvégzett teendők">
+            {doneTasks.length ? (
+              <SimpleGrid columns={columns} padding={6}>
+                {doneTasks.map((task: Task, i) => this.createCard(task))}
+              </SimpleGrid>
+            ) : (
+              <EmptyMessage message="Ma még nincs kész feladat" />
+            )}
+          </Section>
+
+          <Flex justify="flex-end" pt={4}>
+            <Button
+              onClick={() => this.navigate("/newTask")}
+              colorScheme="blue"
+              size="md"
+              borderRadius="full"
+              px={6}
+            >
+              + Új feladat
+            </Button>
+          </Flex>
+        </VStack>
+      </Container>
+    </Hidden>
+    </>
+    );
+  });
 }
+
+
+
