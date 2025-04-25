5ac62e89f4d167b59826e3f4a034ae3c1d5713ff
markobuborek <markokanadateam2@gmail.com>
2025-04-26 00:14:29 +0200

    Frontend: EmptyMessage component

---
+++ frontend/src/common/EmptyText/index.tsx
@@ -0,0 +1,6 @@
+import {Text} from "@chakra-ui/react"
+export const EmptyMessage = ({ message }: { message: string }) => (
+    <Text color="gray.500" fontStyle="italic">
+      {message}
+    </Text>
+  );
