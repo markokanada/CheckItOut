4074c8604ce8af1dcc07b2aba6f8ab77740cff2d
markobuborek <markokanadateam2@gmail.com>
2025-04-28 23:20:14 +0200

    Frontend: Desktop View for tasks

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Center,
   Container,
   Flex,
   HStack,
@@ -17,6 +18,7 @@ import GlobalEntities from "../store/GlobalEntities";
 import { BaseCard } from "../components/Card";
 import { observer } from "mobx-react-lite";
 import { Divider } from "antd";
+
 import { Hidden } from "@mui/material";
 import { EmptyMessage } from "../common/EmptyText";
 import { Section } from "../common/Section";
@@ -42,7 +44,44 @@ export default class Home implements ViewComponent {
     const columns = useBreakpointValue({ base: 1, md: 2, lg: 2, xl: 3 });
 
     return (<>
-              <Hidden lgDown></Hidden>
+              <Hidden lgDown>
+              <Flex alignItems="center" height="100%" paddingTop="2rem">
+  <Box flex="1" p={4}>
+  
+  <Section title="Következő teendő">
+            {this.card ?? <EmptyMessage message="Nincs megjeleníthető feladat" />}
+          </Section>
+  </Box>
+
+
+
+  <Box flex="1" p={4} borderLeft="2px lightgray solid" borderRight="2px lightgray solid">
+  <Section title="Mai teendők">
+            {tasks.length ? (
+              <SimpleGrid columns={columns} padding={6}>
+                {tasks.map((task: Task, i) => this.createCard(task))}
+              </SimpleGrid>
+            ) : (
+              <EmptyMessage message="Nincs aktuális feladat" />
+            )}
+          </Section>
+
+  </Box>
+
+
+  <Box flex="1" p={4}>
+  <Section title="Ma elvégzett teendők">
+            {doneTasks.length ? (
+              <SimpleGrid columns={columns} padding={6}>
+                {doneTasks.map((task: Task, i) => this.createCard(task))}
+              </SimpleGrid>
+            ) : (
+              <EmptyMessage message="Ma még nincs kész feladat" />
+            )}
+          </Section>
+  </Box>
+</Flex>
+              </Hidden>
     <Hidden lgUp>
 
 
