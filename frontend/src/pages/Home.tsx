b8ca4e7e812ec417f55f0ed64afa68d3731077ea
markobuborek <markokanadateam2@gmail.com>
2025-04-29 00:52:17 +0200

    Frontend: Update home with language support

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,9 @@
 import {
   Box,
   Button,
-  Center,
   Container,
   Flex,
-  HStack,
-  Heading,
   SimpleGrid,
-  Text,
   VStack,
   useBreakpointValue,
 } from "@chakra-ui/react";
@@ -18,10 +14,10 @@ import GlobalEntities from "../store/GlobalEntities";
 import { BaseCard } from "../components/Card";
 import { observer } from "mobx-react-lite";
 import { Divider } from "antd";
-
 import { Hidden } from "@mui/material";
 import { EmptyMessage } from "../common/EmptyText";
 import { Section } from "../common/Section";
+import { useTranslation } from "react-i18next";
 
 export default class Home implements ViewComponent {
   constructor(public navigate: NavigateFunction) {
@@ -38,101 +34,82 @@ export default class Home implements ViewComponent {
   }
 
   View = observer(() => {
+    const { t } = useTranslation();
     const tasks = toJS(GlobalEntities.tasks);
     const doneTasks = toJS(GlobalEntities.doneTasks);
 
     const columns = useBreakpointValue({ base: 1, md: 2, lg: 2, xl: 3 });
 
-    return (<>
-              <Hidden lgDown>
-              <Flex alignItems="center" height="100%" paddingTop="2rem">
-  <Box flex="1" p={4}>
-  
-  <Section title="Következő teendő">
-            {this.card ?? <EmptyMessage message="Nincs megjeleníthető feladat" />}
-          </Section>
-  </Box>
+    return (
+      <>
+        <Hidden lgDown>
+          <Flex alignItems="center" height="100%" paddingTop="2rem">
+            <Box flex="1" p={4}>
+              <Section title={t("Next Task Title")}>
+                {this.card ?? <EmptyMessage message={t("Next Task Message")} />}
+              </Section>
+            </Box>
 
+            <Box flex="1" p={4} borderLeft="2px lightgray solid" borderRight="2px lightgray solid">
+              <Section title={t("Today Task Title")}>
+                {tasks.length ? (
+                  <SimpleGrid columns={columns} padding={6}>
+                    {tasks.map((task: Task) => this.createCard(task))}
+                  </SimpleGrid>
+                ) : (
+                  <EmptyMessage message={t("Today Task Message")} />
+                )}
+              </Section>
+            </Box>
 
-
-  <Box flex="1" p={4} borderLeft="2px lightgray solid" borderRight="2px lightgray solid">
-  <Section title="Mai teendők">
-            {tasks.length ? (
-              <SimpleGrid columns={columns} padding={6}>
-                {tasks.map((task: Task, i) => this.createCard(task))}
-              </SimpleGrid>
-            ) : (
-              <EmptyMessage message="Nincs aktuális feladat" />
-            )}
-          </Section>
-
-  </Box>
-
-
-  <Box flex="1" p={4}>
-  <Section title="Ma elvégzett teendők">
-            {doneTasks.length ? (
-              <SimpleGrid columns={columns} padding={6}>
-                {doneTasks.map((task: Task, i) => this.createCard(task))}
-              </SimpleGrid>
-            ) : (
-              <EmptyMessage message="Ma még nincs kész feladat" />
-            )}
-          </Section>
-  </Box>
-</Flex>
-              </Hidden>
-    <Hidden lgUp>
-
-
-    <Container maxW="6xl" py={{ base: 6, md: 8 }}>
-        <VStack padding={10} align="stretch">
-          <Section title="Következő teendő">
-            {this.card ?? <EmptyMessage message="Nincs megjeleníthető feladat" />}
-          </Section>
-
-          <Divider />
-
-          <Section title="Mai teendők">
-            {tasks.length ? (
-              <SimpleGrid columns={columns} padding={6}>
-                {tasks.map((task: Task, i) => this.createCard(task))}
-              </SimpleGrid>
-            ) : (
-              <EmptyMessage message="Nincs aktuális feladat" />
-            )}
-          </Section>
-
-          <Divider />
-
-          <Section title="Ma elvégzett teendők">
-            {doneTasks.length ? (
-              <SimpleGrid columns={columns} padding={6}>
-                {doneTasks.map((task: Task, i) => this.createCard(task))}
-              </SimpleGrid>
-            ) : (
-              <EmptyMessage message="Ma még nincs kész feladat" />
-            )}
-          </Section>
-
-          <Flex justify="flex-end" pt={4}>
-            <Button
-              onClick={() => this.navigate("/newTask")}
-              colorScheme="blue"
-              size="md"
-              borderRadius="full"
-              px={6}
-            >
-              + Új feladat
-            </Button>
+            <Box flex="1" p={4}>
+              <Section title={t("Done Task Title")}>
+                {doneTasks.length ? (
+                  <SimpleGrid columns={columns} padding={6}>
+                    {doneTasks.map((task: Task) => this.createCard(task))}
+                  </SimpleGrid>
+                ) : (
+                  <EmptyMessage message={t("Done Task Message")} />
+                )}
+              </Section>
+            </Box>
           </Flex>
-        </VStack>
-      </Container>
-    </Hidden>
-    </>
+        </Hidden>
+
+        <Hidden lgUp>
+          <Container maxW="6xl" py={{ base: 6, md: 8 }}>
+            <VStack padding={10} align="stretch">
+              <Section title={t("Next Task Title")}>
+                {this.card ?? <EmptyMessage message={t("Next Task Message")} />}
+              </Section>
+
+              <Divider />
+
+              <Section title={t("Today Task Title")}>
+                {tasks.length ? (
+                  <SimpleGrid columns={columns} padding={6}>
+                    {tasks.map((task: Task) => this.createCard(task))}
+                  </SimpleGrid>
+                ) : (
+                  <EmptyMessage message={t("Today Task Message")} />
+                )}
+              </Section>
+
+              <Divider />
+
+              <Section title={t("Done Task Title")}>
+                {doneTasks.length ? (
+                  <SimpleGrid columns={columns} padding={6}>
+                    {doneTasks.map((task: Task) => this.createCard(task))}
+                  </SimpleGrid>
+                ) : (
+                  <EmptyMessage message={t("Done Task Message")} />
+                )}
+              </Section>
+            </VStack>
+          </Container>
+        </Hidden>
+      </>
     );
   });
 }
-
-
-
