347acdb2341cd4d69c1f266fe203a20d8e07e0a9
markobuborek <markokanadateam2@gmail.com>
2025-04-29 15:02:43 +0200

    Frontend: taskRecording to use the component

--- frontend/src/pages/taskRecording.tsx
+++ frontend/src/pages/taskRecording.tsx
@@ -29,68 +29,8 @@ import { useTranslation } from "react-i18next";
 import ViewComponent from "../interfaces/ViewComponent";
 import { PrioritySlider } from "../components/PrioritySlider";
 import {Filter} from "bad-words";
+import { CreateCategoryDialog } from "../components/CreateCategoryDialog";
 
-const filter = new Filter();
-
-const CreateCategoryDialog = observer(({ open, onClose, onCreate }: {
-  open: boolean;
-  onClose: () => void;
-  onCreate: (name: string) => Promise<void>;
-}) => {
-  const { t } = useTranslation();
-
-  const validationSchema = Yup.object().shape({
-    name: Yup.string()
-      .required(t('RequiredField'))
-      .test('capitalized', t('MustStartWithCapital'), (value) => {
-        return value ? value[0] === value[0].toUpperCase() : false;
-      })
-      .test('no-bad-words', t('NoInappropriateWords'), (value) => {
-        return !filter.isProfane(value || '');
-      })
-  });
-
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{t('AddNewCategory')}</DialogTitle>
-      <Formik
-        initialValues={{ name: '' }}
-        validationSchema={validationSchema}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          try {
-            await onCreate(values.name);
-            resetForm();
-            onClose();
-          } finally {
-            setSubmitting(false);
-          }
-        }}
-      >
-        {({ isSubmitting, errors, touched }) => (
-          <Form>
-            <DialogContent>
-              <Field
-                as={TextField}
-                name="name"
-                label={t('CategoryName')}
-                fullWidth
-                margin="normal"
-                error={touched.name && !!errors.name}
-                helperText={touched.name && errors.name}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={onClose}>{t('Cancel')}</Button>
-              <Button type="submit" color="primary" disabled={isSubmitting}>
-                {t('Add')}
-              </Button>
-            </DialogActions>
-          </Form>
-        )}
-      </Formik>
-    </Dialog>
-  );
-});
 
 export default class TaskRecording implements ViewComponent {
   @observable accessor category: Category = { id: undefined, name: undefined };
