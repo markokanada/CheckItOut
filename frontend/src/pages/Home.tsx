a4421b66b71ae80180852bc2322ea6b7df68b159
markobuborek <markokanadateam2@gmail.com>
2025-04-29 19:45:39 +0200

    Frontend: Update home timer time

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -44,7 +44,7 @@ export default class Home implements ViewComponent {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      const timer = setTimeout(() => setLoading(false), 300);
+      const timer = setTimeout(() => setLoading(false), 1200);
       return () => clearTimeout(timer);
     }, []);
 
