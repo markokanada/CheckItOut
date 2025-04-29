a0c3a80bcd2ab7fe5e16e0668ce48a0bf2663bd0
markobuborek <markokanadateam2@gmail.com>
2025-04-29 13:25:44 +0200

    Backend: Update TaskSeeder to seed english priorities

--- backend/database/seeders/TaskSeeder.php
+++ backend/database/seeders/TaskSeeder.php
@@ -14,9 +14,9 @@ class TaskSeeder extends Seeder
     public function run(): void
     {
         DB::table('tasks')->insert([
-            ["id" => 1, "priority" => 10, "status" => "új", "category_id" => 7, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-12 15:51:39"],
-            ["id" => 2, "priority" => 10, "status" => "új", "category_id" => 6, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-06 15:51:39"],
-            ["id" => 3, "priority" => 10, "status" => "új", "category_id" => 5, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-05 15:51:39"]
+            ["id" => 1, "priority" => 10, "status" => "new", "category_id" => 7, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-12 15:51:39"],
+            ["id" => 2, "priority" => 10, "status" => "new", "category_id" => 6, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-06 15:51:39"],
+            ["id" => 3, "priority" => 10, "status" => "new", "category_id" => 5, "user_id" => 1, "title" => "Feladat neve", "description" => "Feladat leirasa", "due_date" => "2025-04-05 15:51:39"]
         ]);
     }
 }
