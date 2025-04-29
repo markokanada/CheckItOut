02f3ef41dc3c349cb1c9f2bc79632e058e6cfadd
markobuborek <markokanadateam2@gmail.com>
2025-04-29 16:22:09 +0200

    Frontend: make StyledAutocomplete common component

---
+++ frontend/src/common/StyledAutocomplete/index.tsx
@@ -0,0 +1,10 @@
+import { Autocomplete } from "@mui/material";
+import styled from "styled-components";
+
+export const StyledAutocomplete = styled(Autocomplete<Category, false, false, false>)({
+  '& .MuiOutlinedInput-root': {
+    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderWidth: '1px',
+    },
+  },
+});
