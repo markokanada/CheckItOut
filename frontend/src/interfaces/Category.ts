7bcc7326ff1a98e136853eb615b39fa56bd924a1
markobuborek <markokanadateam2@gmail.com>
2025-04-29 15:15:04 +0200

    Frontend: Update Category interface with undefineds

--- frontend/src/interfaces/Category.ts
+++ frontend/src/interfaces/Category.ts
@@ -1,6 +1,6 @@
 interface Category {
-  id: number ;
-  name: string;
-  lang: string;
-  user_id: number;
+  id: number |undefined ;
+  name: string |undefined;
+  lang: string |undefined;
+  user_id: number |undefined;
 }
