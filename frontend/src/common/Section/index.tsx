b77b110f26e55a4ac750ff79fa652a0ce01ad5c8
markobuborek <markokanadateam2@gmail.com>
2025-04-29 17:54:57 +0200

    Frontend: Section Heading sizing fixed

--- frontend/src/common/Section/index.tsx
+++ frontend/src/common/Section/index.tsx
@@ -1,8 +1,8 @@
 import { Box, Heading } from "@chakra-ui/react";
 
 export const Section = ({ title, children }: { title: string; children: React.ReactNode }) => (
-    <Box>
-      <Heading fontSize="2.5rem" size="md" mb={3}>
+    <Box px={4}>
+      <Heading fontSize="2rem" size="md" mb={3}>
         {title}
       </Heading>
       {children}
