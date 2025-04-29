ade14332310a86750f949949a30b695d14d2f82f
markokanada <markokanadateam2@gmail.com>
2025-04-29 12:34:17 +0000

    Backend: AuthController remove commented section

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -14,67 +14,6 @@ use Illuminate\Auth\Events\Registered;
 
 class AuthController extends Controller
 {
-    // public function register(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:6',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json($validator->errors(), 400);
-    //     }
-
-    //     $user = User::create([
-    //         'name' => $request->name,
-    //         'email' => $request->email,
-    //         'password' => Hash::make($request->password),
-    //     ]);
-
-    //     $token = JWTAuth::fromUser($user);
-
-    //     return response()->json([
-    //         'message' => 'Sikeres regisztráció!',
-    //         'user' => $user,
-    //         'token' => $token
-    //     ], 201);
-    // }
-
-    // public function login(LoginRequest $request)
-    // {
-    //     $credentials = $request->validated();
-
-    //     if (Auth::attempt($credentials)) {
-    //         $token = $request->user()->createToken("app");
-
-    //         return response()->json([
-    //             'message' => 'Sikeres bejelentkezés!',
-    //             'token' => $token->plainTextToken
-    //         ]);
-    //     }
-
-    //     return response()->json([
-    //             "data" => [
-    //                 "message" => "SIKERETELEN belépés",
-    //             ]
-    //         ],401);
-
-    // }
-
-    // public function me()
-    // {
-    //     return response()->json(auth()->user());
-    // }
-
-    // public function logout()
-    // {
-    //     auth()->logout();
-    //     return response()->json(['message' => 'Sikeres kijelentkezés']);
-    // }
-
-
-
     public function login(LoginRequest $request)
     {
         $cred = $request->validated();
