ca93c8d6637c3f48c1f02ce6887cc28bffbcc086
markobuborek <markokanadateam2@gmail.com>
2025-04-29 18:09:27 +0200

    Frontend: Task interface to use TaskStatus

--- frontend/src/interfaces/Task.ts
+++ frontend/src/interfaces/Task.ts
@@ -1,13 +1,12 @@
+import { TaskStatus } from "./TaskStatus";
+
 interface Task {
   id: number;
   title: string;
   description: string;
   due_date: Date;
   priority: number;
-  status: [            "new",
-    "in-progress",
-    "finished",
-    "expired"]
+  status: TaskStatus,
   category_id: number;
-  user_id: number | object;
+  user_id: number;
 }
