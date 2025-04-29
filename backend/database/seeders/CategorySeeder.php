53d27dd4c68c100e9b1db40e1ed03bfb0cc07e35
markobuborek <markokanadateam2@gmail.com>
2025-04-29 13:37:48 +0200

    Backend: Update Category Seeder to seed english and hungarian categories both

--- backend/database/seeders/CategorySeeder.php
+++ backend/database/seeders/CategorySeeder.php
@@ -14,16 +14,27 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ["id" => 1, "category_name" => "Tanulás"],
-            ["id" => 2, "category_name" => "Házi feladat"],
-            ["id" => 3, "category_name" => "Vizsgafelkészülés"],
-            ["id" => 4, "category_name" => "Kutatás"],
-            ["id" => 5, "category_name" => "Olvasás"],
-            ["id" => 6, "category_name" => "Gyakorlás"],
-            ["id" => 7, "category_name" => "Projektmunka"],
-            ["id" => 8, "category_name" => "Előadáskészítés"],
-            ["id" => 9, "category_name" => "Csoportmunka"],
-            ["id" => 10, "category_name" => "Feladatmegoldás"]
+            ["id" => 1, "category_name" => "Tanulás", "lang" => "hu"],
+            ["id" => 2, "category_name" => "Házi feladat", "lang" => "hu"],
+            ["id" => 3, "category_name" => "Vizsgafelkészülés", "lang" => "hu"],
+            ["id" => 4, "category_name" => "Kutatás", "lang" => "hu"],
+            ["id" => 5, "category_name" => "Olvasás", "lang" => "hu"],
+            ["id" => 6, "category_name" => "Gyakorlás", "lang" => "hu"],
+            ["id" => 7, "category_name" => "Projektmunka", "lang" => "hu"],
+            ["id" => 8, "category_name" => "Előadáskészítés", "lang" => "hu"],
+            ["id" => 9, "category_name" => "Csoportmunka", "lang" => "hu"],
+            ["id" => 10, "category_name" => "Feladatmegoldás", "lang" => "hu"],
+
+            ["id" => 11, "category_name" => "Learning", "lang" => "en"],
+            ["id" => 12, "category_name" => "Homework", "lang" => "en"],
+            ["id" => 13, "category_name" => "Exam preparation", "lang" => "en"],
+            ["id" => 14, "category_name" => "Research", "lang" => "en"],
+            ["id" => 15, "category_name" => "Reading", "lang" => "en"],
+            ["id" => 16, "category_name" => "Practice", "lang" => "en"],
+            ["id" => 17, "category_name" => "Project work", "lang" => "en"],
+            ["id" => 18, "category_name" => "Presentation preparation", "lang" => "en"],
+            ["id" => 19, "category_name" => "Group work", "lang" => "en"],
+            ["id" => 20, "category_name" => "Problem solving", "lang" => "en"],
         ]);
     }
 }
