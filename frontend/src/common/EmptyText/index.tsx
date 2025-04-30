0c765b11b2bb8016cb2b7b3ecde4883c5fa5d01e
markokanada <markokanadateam2@gmail.com>
2025-04-30 23:09:32 +0000

    Frontend: Prettier formatted frontend/src/common/EmptyText/index.tsx

--- frontend/src/common/EmptyText/index.tsx
+++ frontend/src/common/EmptyText/index.tsx
@@ -1,6 +1,6 @@
-import {Text} from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react";
 export const EmptyMessage = ({ message }: { message: string }) => (
-    <Text color="gray.500" fontStyle="italic">
-      {message}
-    </Text>
-  );
+  <Text color="gray.500" fontStyle="italic">
+    {message}
+  </Text>
+);
