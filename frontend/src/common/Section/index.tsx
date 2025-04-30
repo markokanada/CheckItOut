47393628b7727b8f5fe7447fc3076a405d6117a2
markokanada <markokanadateam2@gmail.com>
2025-04-30 23:09:32 +0000

    Frontend: Prettier formatted frontend/src/common/Section/index.tsx

--- frontend/src/common/Section/index.tsx
+++ frontend/src/common/Section/index.tsx
@@ -1,10 +1,16 @@
 import { Box, Heading } from "@chakra-ui/react";
 
-export const Section = ({ title, children }: { title: string; children: React.ReactNode }) => (
-    <Box px={4}>
-      <Heading fontSize="2rem" size="md" mb={3}>
-        {title}
-      </Heading>
-      {children}
-    </Box>
-  );
+export const Section = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => (
+  <Box px={4}>
+    <Heading fontSize="2rem" size="md" mb={3}>
+      {title}
+    </Heading>
+    {children}
+  </Box>
+);
