39c2348a31539d2a1e523d772ee7309e0f83780b
markokanada <markokanadateam2@gmail.com>
2025-04-30 22:38:26 +0000

    Backend: New Mail PasswordResetLink

---
+++ backend/app/Mail/PasswordResetLink.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetLink extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $resetLink;
+
+    public function __construct($resetLink)
+    {
+        $this->resetLink = $resetLink;
+    }
+
+    public function build()
+    {
+        return $this->markdown('emails.password-reset')
+                   ->subject('Jelszó visszaállítási link');
+    }
+}
