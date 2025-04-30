d6e8a53023257cb25c55bd20f9d74bb6f132fc67
markokanada <markokanadateam2@gmail.com>
2025-04-30 23:09:32 +0000

    Frontend: Prettier formatted frontend/src/interfaces/Category.ts

--- frontend/src/interfaces/Category.ts
+++ frontend/src/interfaces/Category.ts
@@ -1,6 +1,6 @@
 interface Category {
-  id: number |undefined ;
-  name: string |undefined;
-  lang: string |undefined;
-  user_id: number |undefined;
+  id: number | undefined;
+  name: string | undefined;
+  lang: string | undefined;
+  user_id: number | undefined;
 }
