da9f0456e4968003a6484d2a7c4143434015710e
markokanada <markokanadateam2@gmail.com>
2025-04-30 23:09:32 +0000

    Frontend: Prettier formatted frontend/src/common/StyledTextField/index.tsx

--- frontend/src/common/StyledTextField/index.tsx
+++ frontend/src/common/StyledTextField/index.tsx
@@ -2,9 +2,9 @@ import { TextField } from "@mui/material";
 import styled from "styled-components";
 
 export const StyledTextField = styled(TextField)({
-  '& .MuiOutlinedInput-root': {
-    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderWidth: '1px',
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderWidth: "1px",
     },
   },
 });
