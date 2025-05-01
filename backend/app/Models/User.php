5858bb6cc2156ca7f185b047c52b0e521df4da1b
markokanada <markokanadateam2@gmail.com>
2025-05-01 10:52:22 +0000

    Backend: Update User with casts

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -45,6 +45,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'password_reset_token_created_at' => 'datetime'
         ];
     }
 
